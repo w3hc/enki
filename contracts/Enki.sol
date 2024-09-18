c62b32cfd563d482a70041eda4c853d25f5f3078
julienbrg <julien@strat.cc>
2024-09-18 13:07:35 +0200

    add netspec comments

--- contracts/Enki.sol
+++ contracts/Enki.sol
@@ -3,9 +3,18 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Enki Contract
+/// @author Julien Béranger
+/// @notice This contract manages text and CID storage with owner-only write access
+/// @dev Inherits from OpenZeppelin's Ownable contract for access control
 contract Enki is Ownable {
+    /// @notice Initializes the contract with an owner, initial text, and CID
+    /// @dev The initial owner should be a Governor contract address
+    /// @param initialOwner Address of the initial owner (Governor contract)
+    /// @param _text Initial text to be stored
+    /// @param _cid Initial CID to be stored
     constructor(
-        address initialOwner, // must be a Governor contract address
+        address initialOwner,
         string memory _text,
         string memory _cid
     ) Ownable(initialOwner) {
@@ -13,27 +22,41 @@ contract Enki is Ownable {
         cid = _cid;
     }
 
+    /// @notice Stores the main text content
     string public text;
+
+    /// @notice Stores the Content Identifier (CID)
     string public cid;
 
+    /// @notice Mapping to store logs of changes
+    /// @dev Keys are old values, values are new values
     mapping(string => string) public logs;
 
-    function read() public {
-        // returns the value of text
+    /// @notice Retrieves the current stored text
+    /// @return The current value of the text variable
+    function read() public view returns (string memory) {
+        return text;
     }
 
-    function write() public onlyOwner {
-        /*
+    /// @notice Updates the stored text and CID
+    /// @dev Only the contract owner can call this function
+    /// @param _newText New text to be stored
+    /// @param _newCid New CID to be stored
+    function write(string calldata _newText, string calldata _newCid) public onlyOwner {
+        string storage oldText = text;
+        string storage oldCid = cid;
 
-        - update the value of text
-        - update the value of cid
-        - update the logs mapping with the new values
+        text = _newText;
+        cid = _newCid;
 
-        */
+        logs[oldText] = _newText;
+        logs[oldCid] = _newCid;
     }
 
-    function verify(string calldata _text) public {
-        // checks if the value of text is equal to _text
-        // if yes, return true
+    /// @notice Verifies if the provided text matches the stored text
+    /// @param _text Text to compare against the stored text
+    /// @return Boolean indicating whether the provided text matches the stored text
+    function verify(string calldata _text) public view returns (bool) {
+        return keccak256(abi.encodePacked(text)) == keccak256(abi.encodePacked(_text));
     }
 }
